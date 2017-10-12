590a9a75443c11bd168eb693eef90c751cd105d4
cristoiroberto <cristoiroberto@gmail.com>
2017-10-12 16:28:56 +0300

    Changed HomePageEditor look

--- TeaWebsite/EditHomePage.php
+++ TeaWebsite/EditHomePage.php
@@ -11,5 +11,21 @@ if(isset($_GET["delete"]))
     $homepageController->DeleteContent($_GET["delete"]);
     header('Refresh: 1; url=EditHomePage.php');
 }
-        
-include './Template.php';   
+?>       
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title; ?></title>
+        <link rel="stylesheet" type="text/css" href="Styles/Stylesheet.css" />
+    </head>
+    <body>
+           
+           <div id="content_area">
+               <?php echo $content; ?>
+           </div>
+      
+    </body>
+</html>
+ 
